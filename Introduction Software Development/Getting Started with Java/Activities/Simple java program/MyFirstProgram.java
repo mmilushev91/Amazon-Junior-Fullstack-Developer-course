9ef179aa038fc09105d9cabcfdab6fccbfd04c58
mmilushev91 <milushev_1990@abv.bg>
2025-08-15 11:14:59 +0300

    Update MyFirstProgram.java

--- Introduction Software Development/Getting Started with Java/Activities/Simple java program/MyFirstProgram.java
+++ Introduction Software Development/Getting Started with Java/Activities/Simple java program/MyFirstProgram.java
@@ -1,5 +1,8 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("I'm a Programmer");
+        System.out.println("Let's change code now...");
+        System.out.println("It's what I do!");
     }
+
 }
